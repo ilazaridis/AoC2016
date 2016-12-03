23fcee91c9cc3744043a472803aba260029984cb
Ioannis Lazaridis <ounos@live.com>
2016-12-03 16:34:02 +0100

    day 3 - minor fix

--- 3/3b.php
+++ 3/3b.php
@@ -3,7 +3,7 @@ $inputs = explode(PHP_EOL, file_get_contents('input.txt'));
 $inputs = array_map(function($e) {return preg_split('/\s+/', trim($e));}, $inputs);
 $columns = array_merge(array_column($inputs, 0), array_column($inputs, 1), array_column($inputs, 2));
 foreach ($columns as $key => $sides) {
-    if ($key == 0 || $key % 3 == 0) {
+    if ($key % 3 == 0) {
         if ($columns[$key] + $columns[$key + 1] > $columns[$key + 2] &&
             $columns[$key] + $columns[$key + 2] > $columns[$key + 1] &&
             $columns[$key + 1] + $columns[$key + 2] > $columns[$key]
