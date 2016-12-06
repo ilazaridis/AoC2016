78808954dcd7b018b41b82c38a06d6aa4dde37da
Ioannis Lazaridis <ounos@live.com>
2016-12-06 19:00:43 +0100

    Day 6

---
+++ 06/6a.php
@@ -0,0 +1,11 @@
+<?php
+$inputs = explode(PHP_EOL, file_get_contents('input.txt'));
+$inputs = array_map(function($e) {return str_split($e);}, $inputs);
+$column = 0; $signal = '';
+while (array_filter(array_column($inputs, $column))) {
+    $frequency = array_count_values(array_column($inputs, $column));
+    arsort($frequency);
+    $signal .= array_keys($frequency)[0];
+    $column++;
+}
+echo $signal;
